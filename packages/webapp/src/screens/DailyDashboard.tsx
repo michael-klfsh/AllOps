86e4e3022fdda8ed74c4e71da4b57688216f3831
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-11-14 23:38:05 +0900

    Add email and welcome card

--- packages/webapp/src/screens/DailyDashboard.tsx
+++ packages/webapp/src/screens/DailyDashboard.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import AppLayout from "../components/AppLayout";
 import Weather from "../components/Weather";
 import { Card, CardBody, Row, Col, Container } from "reactstrap";
+import Email from "../components/Email";
 
 const DailyDashboard = () => (
   <AppLayout>
     <Container>
       <Row className="mb-2">
         <Col xs="8">
-          <Card className="mb-1" style={{ height: "calc(50% - 0.25rem)" }}>
-            <CardBody></CardBody>
+          <Card className="mb-2" style={{ height: "calc(50% - 0.25rem)" }}>
+            <CardBody style={{ textAlign: "center", padding: "10px 10px" }}>
+              <h3>Good day and welcome to your Daily Dashboard! 🌟</h3>
+            </CardBody>
           </Card>
 
-          <Card className="mt-1" style={{ height: "calc(50% - 0.25rem)" }}>
-            <CardBody></CardBody>
+          <Card style={{ height: "calc(50% - 0.25rem)" }}>
+            <CardBody style={{ textAlign: "center" }}>
+              <Email />
+            </CardBody>
           </Card>
         </Col>
         <Col xs="4">
-          <Card>
+          <Card style={{ height: "100%" }}>
             <CardBody>
               <Weather />
             </CardBody>
