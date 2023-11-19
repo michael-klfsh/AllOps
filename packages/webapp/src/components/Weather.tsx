67cd6c5ec2e182acf9ac3eaf57186850bf0b0f0c
Michael Kleefisch <michael.kleefisch@rwth-aachen.de>
2023-11-19 13:04:24 +0900

    Request weather data from external service

--- packages/webapp/src/components/Weather.tsx
+++ packages/webapp/src/components/Weather.tsx
@@ -1,70 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Wind from "../../assets/img/wind.svg";
 import WeatherIcon from "./WeatherIcon";
 
 const Weather = ({ children }: { children?: React.ReactNode }) => {
   //Variable
-  const [isOpen, setIsOpen] = useState(false);
+  const [weather, setWeather] = useState("");
+  const [temperature, setTemperature] = useState(0);
+  const [wind, setWind] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
 
-  const data = {
-    coord: {
-      lon: 10.99,
-      lat: 44.34,
-    },
-    weather: [
-      {
-        id: 501,
-        main: "Rain",
-        description: "moderate rain",
-        icon: "10d",
+  const APIKey = process.env.WEATHER_KEY;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (response) => {
+        setLon(response.coords.longitude);
+        setLat(response.coords.latitude);
       },
-    ],
-    base: "stations",
-    main: {
-      temp: 298.48,
-      feels_like: 298.74,
-      temp_min: 297.56,
-      temp_max: 300.05,
-      pressure: 1015,
-      humidity: 64,
-      sea_level: 1015,
-      grnd_level: 933,
-    },
-    visibility: 10000,
-    wind: {
-      speed: 0.62,
-      deg: 349,
-      gust: 1.18,
-    },
-    rain: {
-      "1h": 3.16,
-    },
-    clouds: {
-      all: 100,
-    },
-    dt: 1661870592,
-    sys: {
-      type: 2,
-      id: 2075663,
-      country: "IT",
-      sunrise: 1661834187,
-      sunset: 1661882248,
-    },
-    timezone: 7200,
-    id: 3163858,
-    name: "Zocca",
-    cod: 200,
-  };
+      (error) => console.error(error)
+    );
 
-  const toggle = () => setIsOpen(!isOpen);
-  const weather = "Rain";
-  const temp = 25.12;
-  const wind = 0.62;
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setWeather(json.weather[0].main);
+        setTemperature(Math.floor(json.main.temp - 273.15));
+        setWind(json.wind.speed);
+      })
+      .catch((error) => console.error(error));
+  }, [lat, lon]);
 
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: "75%" }}>
-        <span>{temp}°C</span>
+        <span>{temperature}°C</span>
         <div>
           <Wind />
           <span>{wind}</span>
